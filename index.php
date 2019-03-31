8c0cff17ce560f493d97ae74026d6b46e619405b
AndreLuckyanto <andreluckyanto@gmail.com>
2019-03-31 23:15:09 +0700

    initial

--- index.php
+++ index.php
@@ -1,11 +1,61 @@
 <html>
 <head></head>
-<body></body>
+<body>
+	<canvas id="myChart" width="400" height="400"></canvas>
+</body>
 </html>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js"></script>
 <script>
+	var ctx = document.getElementById('myChart').getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: [],
+			datasets: [{
+				label: '# of Votes',
+				data: [],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+					}
+				}]
+			}
+		}
+	});
+	
+	function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
     var evtSource = new EventSource("https://map-server-aluckyanto.herokuapp.com/api/invoice/all");
     evtSource.onmessage = function (event) {
         var data = JSON.parse(event.data);
         console.log(data);
+		addData(myChart,data.storeName, data.totalPrice);
     }
 </script>
